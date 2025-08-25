76495b8653b16cf8927e5acff8a7d7517bde9222
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2025-08-25 10:54:48 -0300

    Create if-elif-else.py

---
+++ ProjetoSPython/if-elif-else.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite a sua idade: "))
+
+if idade < 13:
+   print("Você ainda é uma criança.")
+elif idade < 18:
+   print("Você é um dolescente.")
+else:
+   print("Você já é um adulto.")
